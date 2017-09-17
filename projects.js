dddf6c220b9a5b8fcc8e7d6c104ddf8982061c73
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2017-09-17 16:05:09 +0200

    config(pi-dab): use js for config instead of json

---
+++ projects.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = [{
+  'name': 'mlenkeit/pi-dab',
+  'dir': '/home/pi/ug/pi-dab',
+  'githubWebhook': 16109011
+}, {
+  'name': 'mlenkeit/ug-pi-pm2',
+  'dir': '/home/pi/ug/ug-pi-pm2',
+  'githubWebhook': 16194414
+}];
